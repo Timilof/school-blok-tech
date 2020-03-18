c534158f39a9906edba284e36fb275a344655557
Timilof <zinctim@gmail.com>
2020-03-18 07:13:28 +0100

    basic nesting formatting with prettier

--- backend/app.js
+++ backend/app.js
@@ -1,162 +1,186 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const mongo = require('mongodb')
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongo = require("mongodb");
 
 const app = express()
-    .use(express.static(__dirname + '/src'))
-    .set('views', 'views');
+  .use(express.static(__dirname + "/src"))
+  .set("views", "views");
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
+const { MongoClient } = require("mongodb");
 
-const {MongoClient} = require('mongodb');
-
-const uri = process.env.DB_NAME
+const uri = process.env.DB_NAME;
 
 let chatData;
 
-async function GetFromDB(collection){
-    const client = new mongo.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
-		await client.connect();
-		const db = client.db('dating-base');
-		const data = await db.collection(`${collection}`).find({}).toArray();
-        return data
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+async function GetFromDB(collection) {
+  const client = new mongo.MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  try {
+    await client.connect();
+    const db = client.db("dating-base");
+    const data = await db
+      .collection(`${collection}`)
+      .find({})
+      .toArray();
+    return data;
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 
-async function removeFromDB(room){
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
-		await client.connect();
-		const db = client.db('dating-base');
-        const deleteDocument = await db.collection("matches").deleteOne({ roomID: room });
-          return deleteDocument;
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+async function removeFromDB(room) {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  try {
+    await client.connect();
+    const db = client.db("dating-base");
+    const deleteDocument = await db
+      .collection("matches")
+      .deleteOne({ roomID: room });
+    return deleteDocument;
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 
-
-async function updateInCollection(nameOfDocument, newValue){
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
-		await client.connect();
-		const db = client.db('dating-base');
-        const updatedDocument = await db.collection("matches").updateOne({roomID:`${nameOfDocument}`}, { $set:{lastMessage:`${newValue}`}});
-          return updatedDocument;
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+async function updateInCollection(nameOfDocument, newValue) {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  try {
+    await client.connect();
+    const db = client.db("dating-base");
+    const updatedDocument = await db
+      .collection("matches")
+      .updateOne(
+        { roomID: `${nameOfDocument}` },
+        { $set: { lastMessage: `${newValue}` } }
+      );
+    return updatedDocument;
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 
+async function createNewCollection(nameOfNewCollection) {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-async function createNewCollection(nameOfNewCollection){
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    try {
-		await client.connect();
-		const db = client.db('dating-base');
-		const newCollection = await db.createCollection(`${nameOfNewCollection}`);
-          
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+  try {
+    await client.connect();
+    const db = client.db("dating-base");
+    const newCollection = await db.createCollection(`${nameOfNewCollection}`);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
-
 
 // createNewCollection('match-Claudia-Janno-5e6fcac84d32897c95566666');
 
+async function writeDb(data, collection) {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-async function writeDb(data, collection){
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    try {
-		await client.connect();
-		const db = client.db('dating-base');
-		const fullDump = await db.collection(`${collection}`).insertOne({
-            from:data.sender,
-            msg:data.msg,
-            time:data.time
-            })
-        return fullDump   
-          
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+  try {
+    await client.connect();
+    const db = client.db("dating-base");
+    const fullDump = await db.collection(`${collection}`).insertOne({
+      from: data.sender,
+      msg: data.msg,
+      time: data.time
+    });
+    return fullDump;
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 
+io.on("connection", function(socket) {
+  // TODO: add a create-collection event
 
-io.on('connection', function(socket){
+  socket.on("open chat", async function(roomID) {
+    chatData = await GetFromDB(roomID);
+    socket.join(roomID);
+    io.to(roomID).emit("open chat", chatData);
+    // socket.emit('open chat', chatData);
+  });
 
-    // TODO: add a create-collection event 
-
-    socket.on('open chat', async function(roomID){
-        chatData = await GetFromDB(roomID);
-        socket.join(roomID);
-        io.to(roomID).emit('open chat', chatData);
-        // socket.emit('open chat', chatData);
-    })
-
-    // creates a new collection and writes the text message in it
-    socket.on('make new chat', async function(data){
-        chatData = await createNewCollection(data.room);
-        writeDb(data, data.room);
-        updateInCollection(data.room, data.msg);
-        // io.emit('chat message', {from: data.sender, msg:data.msg, time:data.time, room:data.room});
-        io.to(data.room).emit('chat message', {from: data.sender, msg:data.msg, time:data.time, room:data.room});
+  // creates a new collection and writes the text message in it
+  socket.on("make new chat", async function(data) {
+    chatData = await createNewCollection(data.room);
+    writeDb(data, data.room);
+    updateInCollection(data.room, data.msg);
+    // io.emit('chat message', {from: data.sender, msg:data.msg, time:data.time, room:data.room});
+    io.to(data.room).emit("chat message", {
+      from: data.sender,
+      msg: data.msg,
+      time: data.time,
+      room: data.room
     });
+  });
 
-    // normal message handler
-    socket.on('chat message', function(data){
-        writeDb(data, data.room);
-        updateInCollection(data.room, data.msg);
-        // io.emit('chat message', {from: data.sender, msg:data.msg, time:data.time, room:data.room});
-        io.to(data.room).emit('chat message', {from: data.sender, msg:data.msg, time:data.time, room:data.room});
+  // normal message handler
+  socket.on("chat message", function(data) {
+    writeDb(data, data.room);
+    updateInCollection(data.room, data.msg);
+    // io.emit('chat message', {from: data.sender, msg:data.msg, time:data.time, room:data.room});
+    io.to(data.room).emit("chat message", {
+      from: data.sender,
+      msg: data.msg,
+      time: data.time,
+      room: data.room
     });
+  });
 
-    // delete message handler
-    socket.on('leave room', function(room){
-        socket.leave(room);
-    });
-    
-    socket.on('unmatch', function(room){
-        socket.leave(room);
-        removeFromDB(room);
-    });
-    
+  // delete message handler
+  socket.on("leave room", function(room) {
+    socket.leave(room);
+  });
+
+  socket.on("unmatch", function(room) {
+    socket.leave(room);
+    removeFromDB(room);
+  });
 });
 
-app.set('view engine', 'ejs');
-  app.get('/', async (req, res, next) => {
-      const newMatches =[];
-      const oldMatches = [];
-    const matches = await GetFromDB('matches');
-    matches.forEach(match =>{
-        if(match.lastMessage == ""){
-            newMatches.push(match);
-        }else{
-            oldMatches.push(match);
-        }
-    })
-    res.render('chat.ejs', {oldMatches: oldMatches, newMatches: newMatches})
+app.set("view engine", "ejs");
+app.get("/", async (req, res, next) => {
+  const newMatches = [];
+  const oldMatches = [];
+  const matches = await GetFromDB("matches");
+  matches.forEach(match => {
+    if (match.lastMessage == "") {
+      newMatches.push(match);
+    } else {
+      oldMatches.push(match);
+    }
+  });
+  res.render("chat.ejs", { oldMatches: oldMatches, newMatches: newMatches });
 });
 
-
-http.listen((process.env.PORT || port), () => console.log(`Dating app on ${port}!`));
+http.listen(process.env.PORT || port, () =>
+  console.log(`Dating app on ${port}!`)
+);
--- backend/src/scripts/client-socket.js
+++ backend/src/scripts/client-socket.js
@@ -1,225 +1,223 @@
 (function() {
-var socket = io();
+  var socket = io();
 
-const backButton = document.querySelector('.back-button');
-const unMatchHeart = document.querySelector('.unmatch-heart');
-const unmatchQuestion = document.querySelector('.unmatch-question');
-const unmatchPopup = document.querySelector('.unmatch-popup');
-const unmatchPerson = document.querySelector('.unmatched-person');
-const yesUnmatch = document.querySelector('.yea-unmatch');
-const dontUnmatch = document.querySelector('.dont-unmatch');
-const sendButton = document.querySelector('.send');
-const chatArea = document.querySelector('.chat-area');
-const chatWindow = document.querySelector('.chat-container');
-const chatLinks = document.querySelectorAll('.match-block-link');
-const newmachtChatLinks = document.querySelectorAll('.new-match-container-item');
-const jannoBlock = document.querySelector('.janno-block');
+  const backButton = document.querySelector(".back-button");
+  const unMatchHeart = document.querySelector(".unmatch-heart");
+  const unmatchQuestion = document.querySelector(".unmatch-question");
+  const unmatchPopup = document.querySelector(".unmatch-popup");
+  const unmatchPerson = document.querySelector(".unmatched-person");
+  const yesUnmatch = document.querySelector(".yea-unmatch");
+  const dontUnmatch = document.querySelector(".dont-unmatch");
+  const sendButton = document.querySelector(".send");
+  const chatArea = document.querySelector(".chat-area");
+  const chatWindow = document.querySelector(".chat-container");
+  const chatLinks = document.querySelectorAll(".match-block-link");
+  const newmachtChatLinks = document.querySelectorAll(
+    ".new-match-container-item"
+  );
+  const jannoBlock = document.querySelector(".janno-block");
 
-const chatTitle = document.querySelector('.chat-name');
-const chatImage = document.querySelector('.chat-img');
+  const chatTitle = document.querySelector(".chat-name");
+  const chatImage = document.querySelector(".chat-img");
 
-const inputField = document.querySelector('.input');
+  const inputField = document.querySelector(".input");
 
+  let userName = "Janno";
+  let roomId;
+  let talkingTo;
+  let isNewMatch = false;
 
-let userName = 'Janno';
-let roomId;
-let talkingTo;
-let isNewMatch = false;
+  // {
+  //     "name":"saskia",
+  //     "roomID":"mattch",
+  //     "herImage":"hhh",
+  //     "hisImage":"",
+  //     "lastMessage":""
+  //     }
+  // {
+  //     "name":"Charlie",
+  //     "roomID":"match-charlie-Janno-5e6fbd3f38f8fc7b1ce48165",
+  //     "herImage":"https://i.redd.it/i3qlw1aid7g21.jpg",
+  //     "hisImage":"https://avatars1.githubusercontent.com/u/11157347?s=460&v=4",
+  //     "lastMessage":"chachacha",
+  //      "hisName":"Janno"
+  //     }
+  // {
+  //     "name":"Claudia",
+  //     "roomID":"match-Claudia-Janno-5e6fcac84d32897c95566666",
+  //     "herImage":"https://thumbor.pijper.io/RaHRNIppbLZxvOdCjNmSIdeUNBc=/1290x726/center/middle/https://cdn.pijper.io/2019/12/w5m5CsnDylIwC71576744636.jpeg",
+  //     "hisName":"Janno",
+  //     "hisImage":"https://avatars1.githubusercontent.com/u/11157347?s=460&v=4",
+  //     "lastMessage":""
+  //     }
 
-// {
-//     "name":"saskia",
-//     "roomID":"mattch",
-//     "herImage":"hhh",
-//     "hisImage":"",
-//     "lastMessage":""
-//     }
-// {
-//     "name":"Charlie",
-//     "roomID":"match-charlie-Janno-5e6fbd3f38f8fc7b1ce48165",
-//     "herImage":"https://i.redd.it/i3qlw1aid7g21.jpg",
-//     "hisImage":"https://avatars1.githubusercontent.com/u/11157347?s=460&v=4",
-//     "lastMessage":"chachacha",
-//      "hisName":"Janno"
-//     }
-// {
-//     "name":"Claudia",
-//     "roomID":"match-Claudia-Janno-5e6fcac84d32897c95566666",
-//     "herImage":"https://thumbor.pijper.io/RaHRNIppbLZxvOdCjNmSIdeUNBc=/1290x726/center/middle/https://cdn.pijper.io/2019/12/w5m5CsnDylIwC71576744636.jpeg",
-//     "hisName":"Janno",
-//     "hisImage":"https://avatars1.githubusercontent.com/u/11157347?s=460&v=4",
-//     "lastMessage":""
-//     }
-
-function scrollToEnd(){
+  function scrollToEnd() {
     chatWindow.scrollTop = chatWindow.scrollHeight;
-}
+  }
 
-function buildAMessage(sender, messageContent, timestamp){
+  function buildAMessage(sender, messageContent, timestamp) {
     let setToSide;
-    sender == userName? setToSide = 'my-message': setToSide = '';
+    sender == userName ? (setToSide = "my-message") : (setToSide = "");
     let aMessage = `<li class="message ${setToSide}"><p>${messageContent}</p><span>${timestamp}</span></li>`;
     return aMessage;
-}
+  }
 
-function updateLastMessages(lastMessage, room){
-    chatLinks.forEach(chatLink =>{
-        if(chatLink.dataset.room == room){
-            chatLink.querySelector('.last-message').textContent = lastMessage;
-        }
+  function updateLastMessages(lastMessage, room) {
+    chatLinks.forEach(chatLink => {
+      if (chatLink.dataset.room == room) {
+        chatLink.querySelector(".last-message").textContent = lastMessage;
+      }
     });
-}
+  }
 
-function clearInputfieldAndFocus(){
-    document.querySelector('.input').value = "";
-    document.querySelector('.input').focus();
+  function clearInputfieldAndFocus() {
+    document.querySelector(".input").value = "";
+    document.querySelector(".input").focus();
     //maybe change the placeholder depending on input? user feedback iets
-}
+  }
 
-function getCurrentTime(){
-    let minutes = new Date().getMinutes()
-    let hours = new Date().getHours()
+  function getCurrentTime() {
+    let minutes = new Date().getMinutes();
+    let hours = new Date().getHours();
     const timestamp = hours + ":" + minutes;
-    return timestamp
-}
+    return timestamp;
+  }
 
-function setImageAndName(img, name){
+  function setImageAndName(img, name) {
     chatTitle.textContent = name;
     chatImage.src = img;
-}
+  }
 
-function renderMessagePrompt(){
+  function renderMessagePrompt() {
     const sendMessagePrompt = `<li class="prompt"><p>Send a message and she might actually reply</p></li>`;
     chatWindow.innerHTML = "";
-    chatWindow.insertAdjacentHTML('beforeend', sendMessagePrompt)
-}
+    chatWindow.insertAdjacentHTML("beforeend", sendMessagePrompt);
+  }
 
-function addChatListeners(chatLink, newMatch){
-    chatLink.addEventListener('click', (e)=>{
-        e.preventDefault();
-        roomId = e.target.dataset.room
-        let matchName = e.target.dataset.name
-        let matchImg = e.target.dataset.img
-        talkingTo = e.target.dataset.name;
-        setImageAndName(matchImg, matchName)
-        chatArea.classList.toggle('down');
-        if(talkingTo == "Janno"){
-            userName = "Charlie"
-        }
-        if(newMatch){
-            isNewMatch = true;
-            renderMessagePrompt();
-        }else{
-            isNewMatch = false;
-            socket.emit('open chat', roomId); 
-        };
-    })
-}
-
-newmachtChatLinks.forEach(chat =>{
-    addChatListeners(chat, true);
-})
-chatLinks.forEach(chat =>{
-    addChatListeners(chat, false);
-})
-addChatListeners(jannoBlock);
-
-backButton.addEventListener('click', (e)=>{
-    e.preventDefault();
-    if(isNewMatch == 'reload'){
-        location.reload();
-    }
-    socket.emit('leave room', roomId);
-    chatArea.classList.toggle('down');
-});
-
-
-unMatchHeart.addEventListener('click', (e)=>{
-    e.preventDefault();
-    unmatchQuestion.classList.toggle('hidden');
-})
-
-yesUnmatch.addEventListener('click', (e)=>{
-    e.preventDefault();
-    chatLinks.forEach(chatLink =>{
-        if(chatLink.dataset.room == roomId){
-            chatLink.remove();
-        }
+  function addChatListeners(chatLink, newMatch) {
+    chatLink.addEventListener("click", e => {
+      e.preventDefault();
+      roomId = e.target.dataset.room;
+      let matchName = e.target.dataset.name;
+      let matchImg = e.target.dataset.img;
+      talkingTo = e.target.dataset.name;
+      setImageAndName(matchImg, matchName);
+      chatArea.classList.toggle("down");
+      if (talkingTo == "Janno") {
+        userName = "Charlie";
+      }
+      if (newMatch) {
+        isNewMatch = true;
+        renderMessagePrompt();
+      } else {
+        isNewMatch = false;
+        socket.emit("open chat", roomId);
+      }
     });
-    unmatchQuestion.classList.toggle('hidden');
-    chatArea.classList.toggle('down');
-    unmatchPopup.classList.toggle('hidden')
+  }
+
+  newmachtChatLinks.forEach(chat => {
+    addChatListeners(chat, true);
+  });
+  chatLinks.forEach(chat => {
+    addChatListeners(chat, false);
+  });
+  addChatListeners(jannoBlock);
+
+  backButton.addEventListener("click", e => {
+    e.preventDefault();
+    if (isNewMatch == "reload") {
+      location.reload();
+    }
+    socket.emit("leave room", roomId);
+    chatArea.classList.toggle("down");
+  });
+
+  unMatchHeart.addEventListener("click", e => {
+    e.preventDefault();
+    unmatchQuestion.classList.toggle("hidden");
+  });
+
+  yesUnmatch.addEventListener("click", e => {
+    e.preventDefault();
+    chatLinks.forEach(chatLink => {
+      if (chatLink.dataset.room == roomId) {
+        chatLink.remove();
+      }
+    });
+    unmatchQuestion.classList.toggle("hidden");
+    chatArea.classList.toggle("down");
+    unmatchPopup.classList.toggle("hidden");
     setTimeout(() => {
-        unmatchPopup.classList.toggle('hidden')
+      unmatchPopup.classList.toggle("hidden");
     }, 2000);
     unmatchPerson.textContent = talkingTo;
-    socket.emit('unmatch', roomId);
-})
-dontUnmatch.addEventListener('click', (e)=>{
+    socket.emit("unmatch", roomId);
+  });
+  dontUnmatch.addEventListener("click", e => {
     e.preventDefault();
-    unmatchQuestion.classList.toggle('hidden');
-})
+    unmatchQuestion.classList.toggle("hidden");
+  });
 
-
-socket.on('open chat', function(data){
-    chatWindow.innerHTML = '';
+  socket.on("open chat", function(data) {
+    chatWindow.innerHTML = "";
     data.forEach(messageObject => {
-        const messageToRender = buildAMessage(messageObject.from, messageObject.msg, messageObject.time);
-        chatWindow.insertAdjacentHTML('beforeend', messageToRender);
+      const messageToRender = buildAMessage(
+        messageObject.from,
+        messageObject.msg,
+        messageObject.time
+      );
+      chatWindow.insertAdjacentHTML("beforeend", messageToRender);
     });
     scrollToEnd();
-});
+  });
 
-socket.on('chat message', function(data){
-    if(isNewMatch){
-        chatWindow.innerHTML = "";
+  socket.on("chat message", function(data) {
+    if (isNewMatch) {
+      chatWindow.innerHTML = "";
     }
     const messageToRender = buildAMessage(data.from, data.msg, data.time);
-    chatWindow.insertAdjacentHTML('beforeend', messageToRender);
+    chatWindow.insertAdjacentHTML("beforeend", messageToRender);
     scrollToEnd();
     updateLastMessages(data.msg, data.room);
-});
+  });
 
-
-
-sendButton.addEventListener("click", function(e) {
+  sendButton.addEventListener("click", function(e) {
     e.preventDefault();
     sendMessage();
-});
+  });
 
-inputField.addEventListener("keydown", function(e) {
-    if(e.key == 'Enter'){
-        e.preventDefault();
-        sendMessage();
+  inputField.addEventListener("keydown", function(e) {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      sendMessage();
     }
-});
+  });
 
+  function sendMessage() {
+    const message = inputField.value;
+    if (message.length < 1) {
+      clearInputfieldAndFocus();
+      return;
+    }
 
-
-function sendMessage(){
-    const message = inputField.value
-    if(message.length < 1){
-            clearInputfieldAndFocus();
-            return
-        }
-        
-            const timestamp = getCurrentTime();
-            if(isNewMatch){
-                isNewMatch = 'reload';
-                socket.emit('make new chat', {
-                    sender: userName,
-                    msg: message,
-                    time: timestamp,
-                    room: roomId
-                  });    
-            }else{
-                socket.emit('chat message', {
-                    sender: userName,
-                    msg: message,
-                    time: timestamp,
-                    room: roomId
-                  });
-            }
-            clearInputfieldAndFocus();
-}
-
-}());
+    const timestamp = getCurrentTime();
+    if (isNewMatch) {
+      isNewMatch = "reload";
+      socket.emit("make new chat", {
+        sender: userName,
+        msg: message,
+        time: timestamp,
+        room: roomId
+      });
+    } else {
+      socket.emit("chat message", {
+        sender: userName,
+        msg: message,
+        time: timestamp,
+        room: roomId
+      });
+    }
+    clearInputfieldAndFocus();
+  }
+})();
